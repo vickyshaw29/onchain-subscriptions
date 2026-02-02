ae5c24f4b0b066d093bf62fa1707d643752dc2c9
Vicky Kumar Prasad <prasadvicky231@gmail.com>
2026-02-02 17:30:46 +0530

    added create subscriptionn with reusable modifiers

--- contracts/src/OnchainSubscriptions.sol
+++ contracts/src/OnchainSubscriptions.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.20;
 import { IOnChainSubscriptions } from "./interfaces/IOnchainSubscriptions.sol";
 import { Subscription } from "./types/Subscription.sol";
 
+
 contract OnChainSubscriptions is IOnChainSubscriptions {
     error InvalidSubscription();
     error NotSubscriber();
@@ -13,6 +14,12 @@ contract OnChainSubscriptions is IOnChainSubscriptions {
     error SubscriptionExpired();
     error ChargeTooEarly();
     error TransferFailed();
+    error ZeroAddress();
+    error ZeroAmount();
+    error InvalidInterval();
+    error InvalidTimeWindow();
+    error SubscriptionAlreadyExists();
+
 
     // EVENTS
     event Charged(
@@ -37,10 +44,54 @@ contract OnChainSubscriptions is IOnChainSubscriptions {
             );
         }
 
+    // modifiers 
+
+    modifier validAddress(address addr){
+        if(addr == address(0)){
+            revert ZeroAddress();
+        }
+        _;
+    }
+
+    modifier nonZero(uint256 value){
+        if(value == 0){
+            revert ZeroAmount();
+        }
+        _;
+    }
+
+    modifier validTimeWindow(uint256 startTime, uint256 endTime){
+        if(startTime < block.timestamp){
+            revert InvalidInterval();
+        }
+        if(endTime !=0 && endTime <= startTime){
+            revert InvalidTimeWindow();
+        }
+        _;
+    }
+
     function createSubscription(
         address merchant, address token, uint256 amount, uint256 interval, uint256 startTime, uint256 endTime, uint256 salt
-        ) external override returns(bytes32 subscriptionId){
-            //
+        ) external override validAddress(merchant) validAddress(token) nonZero(amount) nonZero(interval) validTimeWindow(startTime, endTime) returns(bytes32 subscriptionId){
+            subscriptionId = getSubscriptionId(msg.sender, merchant, token, salt);
+            if(subscriptions[subscriptionId].subscriber != address(0)){
+                revert SubscriptionAlreadyExists(); 
+            }
+
+            subscriptions[subscriptionId] = Subscription({
+                subscriber: msg.sender,
+                merchant: merchant,
+                token: token,
+                amount: amount,
+                interval: interval,
+                startTime: startTime,
+                endTime: endTime,
+                lastChargedtAt: 0,
+                revoked: false 
+            });
+
+
+            return subscriptionId;
         }
 
     function charge(bytes32 subscriptionId) external override {
